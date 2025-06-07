d6887664a1ad7768774a211050e9f2753b311546
risvaaphillips09 <risvaa83@gmail.com>
2025-06-07 18:15:39 -0400

    change a file

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World from Indiana, Bloomington!!");
+    return res.send("Hello World from Indiana, Bloomington!");
 
 app.get('/api', function (req, res) {
     return res.send("Fabrikam Bank API");
